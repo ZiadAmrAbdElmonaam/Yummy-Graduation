cf6feb069fa5aa9faea1d55b17832e08cd4daf2e
mohamedElnagar79 <mohamedelnaggar002@gmail.com>
2022-08-06 12:01:34 +0200

    add comment to auth in orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,97 +3,97 @@ require("../models/orderSchema");
 const Order = mongoose.model("orders");
 
 module.exports.getAllOrder = (req, res, next) => {
-    Order.find({})
-        .then(data => {
-            res.status(200).json(data)
-        })
-        .catch(error => {
-            next(error)
-        })
-}
+  Order.find({})
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 module.exports.getOrderById = (req, res, next) => {
-    id = req.params.id
-    Order.findById(id)
-        .then(data => {
-            res.status(200).json(data)
-        })
-        .catch(error => {
-            next(error)
-        })
-}
+  id = req.params.id;
+  Order.findById(id)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 module.exports.createNewOrder = (req, res, next) => {
-    let orderObject = new Order({
+  let orderObject = new Order({
+    totalPrice: req.body.totalPrice,
+    userid: req.body.userid,
+    kitchen: req.body.kitchen,
+    orderItems: req.body.orderItems,
+  });
+  orderObject
+    .save()
+    .then((data) => {
+      res.status(201).json({ data: data });
+    })
+    .catch((error) => next(error));
+};
+
+module.exports.updateOrderById = (req, res, next) => {
+  Order.updateOne(
+    { _id: req.params.id },
+    {
+      $push: {
+        orderItems: {
+          $each: req.body.orderItems,
+        },
+      },
+      $set: {
         totalPrice: req.body.totalPrice,
         userid: req.body.userid,
         kitchen: req.body.kitchen,
-        orderItems: req.body.orderItems
+        deliverypilot: req.body.deliverypilot,
+        kitchenOrderStatus: req.body.kitchenOrderStatus,
+        pilotOrderStatus: req.body.pilotOrderStatus,
+      },
+    }
+  )
+
+    .then((data) => {
+      // console.log(data.modifiedCount);
+      if (data.modifiedCount == 0) next(new Error("Item not found"));
+      else res.status(200).json({ data: "updated" });
+    })
+    .catch((error) => {
+      next(error);
     });
-    orderObject.save()
-        .then((data) => {
-            res.status(201).json({ "data": data });
-        })
-        .catch(error => next(error))
-}
-
-
-module.exports.updateOrderById = (req, res, next) => {
-    Order.updateOne({ _id: req.params.id },
-        {
-            $push: {
-                orderItems: {
-                    $each: req.body.orderItems
-                }
-            },
-            $set: {
-                totalPrice: req.body.totalPrice,
-                userid: req.body.userid,
-                kitchen: req.body.kitchen,
-                deliverypilot: req.body.deliverypilot,
-                kitchenOrderStatus: req.body.kitchenOrderStatus,
-                pilotOrderStatus:req.body.pilotOrderStatus
-            }
-
-        })
-
-        .then(data => {
-            // console.log(data.modifiedCount);
-            if (data.modifiedCount == 0) next(new Error("Item not found"));
-            else
-                res.status(200).json({ data: "updated" })
-        })
-        .catch(error => { next(error) })
-
-}
+};
 module.exports.deleteOrderById = (req, res, next) => {
-    Order.deleteOne({ _id: req.params.id })
-        .then(data => {
-            if (data.deletedCount == 0) next(new Error("Item not found"));
-            else
-                res.status(200).json({ data: "deleted" })
-        })
-        .catch(error => { next(error) })
-}
+  Order.deleteOne({ _id: req.params.id })
+    .then((data) => {
+      if (data.deletedCount == 0) next(new Error("Item not found"));
+      else res.status(200).json({ data: "deleted" });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 
 module.exports.deleteOrderItemById = (req, res, next) => {
-    Order.updateOne({ _id: req.params.id },
-        {
-            $pull: {
-                orderItems: {
-                    $in: req.body.orderItems
-                }
-            }
-        })
-        .then(data => {
-            if (data.modifiedCount == 0) next(new Error("Item not found"));
-            else
-                res.status(200).json({ data: "deleted" })
-        })
-        .catch(error => { next(error) })
-}
-
-
-
+  Order.updateOne(
+    { _id: req.params.id },
+    {
+      $pull: {
+        orderItems: {
+          $in: req.body.orderItems,
+        },
+      },
+    }
+  )
+    .then((data) => {
+      if (data.modifiedCount == 0) next(new Error("Item not found"));
+      else res.status(200).json({ data: "deleted" });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 
 // Controller Poplulate orders
-
-
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,67 +1,72 @@
-const orderController = require('../controllers/orderController');
-const express = require('express');
+const orderController = require("../controllers/orderController");
+const express = require("express");
 const router = express.Router();
 const mwError = require("../MW/validatiomMW");
 const authMW = require("../MW/authMW");
-const { orderValidationAdd, orderValidationUpdate, orderValidationDelete, orderItemValidationDelete } = require("../MW/orderMW");
+const {
+  orderValidationAdd,
+  orderValidationUpdate,
+  orderValidationDelete,
+  orderItemValidationDelete,
+} = require("../MW/orderMW");
 
-router.route("/orders")
-    .get(authMW,
-        (req, res, next) => {
-            if (req.role === "admin") {
-                next();
-            }
-            else {
-                res.status(403).json({ msg: "just Admins can access this route " })
-            }
-        }
-        , orderController.getAllOrder)
+router
+  .route("/orders")
+  .get(
+    // authMW,
+    // (req, res, next) => {
+    //   if (req.role === "admin") {
+    //     next();
+    //   } else {
+    //     res.status(403).json({ msg: "just Admins can access this route " });
+    //   }
+    // },
+    orderController.getAllOrder
+  )
 
-    .post(authMW,
-        (req, res, next) => {
-            if (((req.role === "user") || (req.role === "admin"))) {
-                next();
-            }
-            else {
-                res.status(403).json({
-                    message: "You are not authorized to access this resource."
-                });
-            }
-        }, orderValidationAdd, mwError, orderController.createNewOrder)
-router.route("/order/:id")
+  .post(
+    authMW,
+    (req, res, next) => {
+      if (req.role === "user" || req.role === "admin") {
+        next();
+      } else {
+        res.status(403).json({
+          message: "You are not authorized to access this resource.",
+        });
+      }
+    },
+    orderValidationAdd,
+    mwError,
+    orderController.createNewOrder
+  );
+router
+  .route("/order/:id")
 
-    .all(authMW,
-        (req, res, next) => {
-            if (((req.role == "kitchen")
-                || (req.role == "pilot")
-                || (req.role == "user")
-            )
-                && (req.orders.includes(Number(req.params.id)))
-                || (req.role == "admin")
-            ) {
-                console.log("req.orders: ", req.orders);
-                console.log("order request", (req.orders.includes(Number(req.params.id))));
-                next();
-            }
-            else {
-                res.status(403).json({
-                    message: "You are not authorized to access this resource."
-                });
-            }
-        }
+  .all(authMW, (req, res, next) => {
+    if (
+      ((req.role == "kitchen" || req.role == "pilot" || req.role == "user") &&
+        req.orders.includes(Number(req.params.id))) ||
+      req.role == "admin"
+    ) {
+      console.log("req.orders: ", req.orders);
+      console.log("order request", req.orders.includes(Number(req.params.id)));
+      next();
+    } else {
+      res.status(403).json({
+        message: "You are not authorized to access this resource.",
+      });
+    }
+  })
+  .get(mwError, orderController.getOrderById)
+  .put(orderValidationUpdate, mwError, orderController.updateOrderById)
+  .delete(orderValidationDelete, mwError, orderController.deleteOrderById);
 
-    )
-    .get(mwError, orderController.getOrderById)
-    .put(orderValidationUpdate, mwError, orderController.updateOrderById)
-    .delete(orderValidationDelete, mwError, orderController.deleteOrderById)
-
-
-router.route("/orderItems/:id")
-    .delete(orderItemValidationDelete, mwError, orderController.deleteOrderItemById)
-
-
-
+router
+  .route("/orderItems/:id")
+  .delete(
+    orderItemValidationDelete,
+    mwError,
+    orderController.deleteOrderItemById
+  );
 
 module.exports = router;
-
-

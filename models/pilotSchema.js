3948e26d3680560a9e3428a252e929ff4e247915
husseinhegazi <husseinhegazyy778@gmail.com>
2022-08-13 07:36:35 +0200

    edit schema validation

--- models/pilotSchema.js
+++ models/pilotSchema.js
@@ -1,8 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const pilotSchema = new mongoose.Schema({
+const pilotSchema = new mongoose.Schema(
+  {
     _id: { type: Number, required: true, length: 14 },
-    pilotStatus: { type: String, required: true, default: "not avilable", enum: ["avilable", "not avilable"] },
+    pilotStatus: {
+      type: String,
+      required: true,
+      default: "not avilable",
+      enum: ["avilable", "not avilable"],
+    },
     deliveryDeparture: { type: Date, default: Date.now, required: true },
     deliveryArrival: { type: Date, default: Date.now, required: true },
     pilotName: { type: String, required: true, minlength: 3, maxlength: 50 },
@@ -10,15 +16,10 @@ const pilotSchema = new mongoose.Schema({
     pilotNumber: { type: Number, required: true, unique: true, length: 11 },
     pilotNotes: { type: String },
     pilotLisenceImage: { type: String, required: true },
-    pilotPassword: { type: String, required: true, minlength: 8 }
-}
-    , {
-        _id: false
-    }
-)
+    pilotPassword: { type: String, required: true, minlength: 8 },
+  },
+  {
+    _id: false,
+  }
+);
 module.exports = mongoose.model("pilots", pilotSchema);
-
-
-
-
-
